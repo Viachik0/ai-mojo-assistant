6628029c8e7b6c40b6b339e85ae2bd0e3d9e3e88
Slava Klimov <130772081+Viachik0@users.noreply.github.com>
2025-10-15 17:08:08 +0200

    Import User model in student.py

--- app/models/student.py
+++ app/models/student.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from app.models.base import Base
+from app.models.base import Base, User
 
 class Student(Base):
     __tablename__ = "students"
