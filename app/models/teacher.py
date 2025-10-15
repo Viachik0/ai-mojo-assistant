4dbfe540f2d9636107d0f31f68a702c10d00b38b
Slava Klimov <130772081+Viachik0@users.noreply.github.com>
2025-10-15 17:15:27 +0200

    Remove unused import from teacher.py
    Removed unused import of User model.

--- app/models/teacher.py
+++ app/models/teacher.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 from app.models.base import Base
-from app.models.user import User
 
 class Teacher(Base):
     __tablename__ = "teachers"
